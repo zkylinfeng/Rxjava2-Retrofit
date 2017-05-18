6f346e28f5f146cfe1d1c1392e47db1f429ed1bd
zkylinfeng <2425759217@qq.com>
2017-05-18 21:18:09 +0800

    zouky

---
+++ mylibrary/src/main/java/com/android/zouky/mylibrary/base/BaseApplication.java
@@ -0,0 +1,12 @@
+package com.android.zouky.mylibrary.base;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/5/18.
+ */
+
+public class BaseApplication extends Application {
+
+
+}
---
+++ mylibrary/src/main/java/com/android/zouky/mylibrary/util/AppToast.java
@@ -0,0 +1,8 @@
+package com.android.zouky.mylibrary.util;
+
+/**
+ * Created by Administrator on 2017/5/18.
+ */
+
+public class AppToast {
+}
